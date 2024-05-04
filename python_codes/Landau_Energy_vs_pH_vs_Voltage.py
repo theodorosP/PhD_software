e6667dd46733dca80f85fc07628574bbf3491c01
thodorisP <teosfp@hotmail.com>
2024-05-04 16:48:58 -0400

    commit

--- python_codes/Landau_Energy_vs_pH_vs_Voltage.py
+++ python_codes/Landau_Energy_vs_pH_vs_Voltage.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
-import Landau_Energy
+import Landau_Energy_2
 
-obj = Landau_Energy.fix_data( 0, 7 )
+obj = Landau_Energy_2.fix_data( 0, 7 )
 pH, V, Landau_Energy = obj.get_plotting_data()
 
 plt.figure(figsize=(10, 6))
@@ -9,6 +9,4 @@ contour = plt.contourf(V, pH , Landau_Energy, cmap='viridis')
 plt.colorbar(contour, label='eV / $\mathring{A}^2$')
 plt.xlabel(r'($\Phi$ V vs SHE)')
 plt.ylabel('pH')
-#plt.title('?? per H(V, pH)')
-#plt.savefig("Landau_Energy")
 plt.show()
