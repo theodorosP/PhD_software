a16a57c33dfdeac7446f01d8e67aed5e0bff1759
thodorisP <teosfp@hotmail.com>
2024-05-04 16:47:59 -0400

    commit

--- python_codes/Landau_energy.py
+++ python_codes/Landau_energy.py
@@ -14,11 +14,15 @@ class Landau_Energy:
 		self.pH_down = pH_down
 		self.pH_up = pH_up
 		self.loc = os.getcwd()
+		self.energy_H2 = self.get_omega_H2()
 		self.Vol_dir =  self.get_Vol_dir()
 		self.SHE_voltages = self.get_SHE_voltages()
 		self.coverage_dir = self.get_coverage_dir()
 		self.number_of_H = self.get_number_of_H()
 		self.mu_H = self.get_mu_H()
+		self.grand_canonical_energy = self.get_grand_canonical_energy()
+		self.area = self.get_unit_cell_area()
+		self.keys_to_drop = self.get_keys_to_drop()
 		self.dictionary = self.get_Landau_energy()
 
 	def get_Vol_dir( self ):
@@ -64,24 +68,13 @@ class Landau_Energy:
 		area = np.sqrt( area_vec[ 0 ]**2 + area_vec[ 1 ]**2 + area_vec[ 2 ]**2 )
 		return area
 
+	
 	def get_mu_H( self ):
-		energy_H2 = self.get_omega_H2()
 		mu_H = {}
 		for pH in range( self.pH_down, self.pH_up + 1 ):
 			for SHE in self.SHE_voltages:
-				for N in self.coverage_dir:
-					mu_H[ str( pH ) + "_" + str( SHE ) + "_" + str( N ) ] = self.number_of_H[ N ] * ( 0.5 * energy_H2 - self.ln10KT * pH - float( SHE ) )
+				mu_H[ str( pH ) + "_" + str( SHE )  ] =  0.5 * self.energy_H2 - self.ln10KT * pH - float( SHE )
 		return mu_H
-
-	
-	def get_mu_H_meth_2( self ):
-		energy_H2 = self.get_omega_H2()
-		mu_H = {}
-		for pH in [ 7 ]:
-			for SHE in self.SHE_voltages:
-				mu_H[ str( pH ) + "_" + str( SHE ) ] =  0.5 * energy_H2 - self.ln10KT * pH - float( SHE )
-		return mu_H
-
 
 	def get_grand_canonical_energy( self ):
 		grand_canonical_energy = {}
@@ -93,69 +86,46 @@ class Landau_Energy:
 				grand_canonical_energy[ i + "_" + j ] = struc.get_potential_energy()
 		return grand_canonical_energy
 
-	def get_Landau_energy( self ):
-		area = self.get_unit_cell_area()
+	def get_keys_to_drop( self ):
 		keys_to_drop = list()
-		grand_canonical_energy = self.get_grand_canonical_energy()
+		for pH in range( self.pH_down, self.pH_up + 1 ):
+			for SHE in self.SHE_voltages:
+				keys_to_drop.append( str( pH ) + "_" + str( SHE ) + "_" + "0ML" )
+		return keys_to_drop
+	
+
+	def get_Landau_energy( self ):
+		keys_to_drop = list()
 		Landau_energy = {}
-		Landau_energy_per_H = {}
-		energy_H2 = self.get_omega_H2()
 		for SHE in self.SHE_voltages:
 			for coverage in self.coverage_dir:     
-				for pH in range( self.pH_down, self.pH_up + 1 ):                 
-					Landau_energy[ str( pH ) + "_" + str( SHE ) + "_" + str( coverage ) ] = grand_canonical_energy[ "chg_" + str( SHE ) + "_" + str(coverage) ] - grand_canonical_energy[ "chg_" + str( SHE ) + "_0ML"  ] - self.mu_H[ str( pH ) + "_" + str(SHE) + "_" + str(coverage) ]        
-		for pH in range( self.pH_down, self.pH_up + 1 ):
-			for SHE in self.SHE_voltages: 
-				keys_to_drop.append( str( pH ) + "_" + str( SHE ) + "_" + "0ML" )
-		Landau_energy_drop = { key: value for key, value in Landau_energy.items() if key not in keys_to_drop }
-		Landau_energy_per_area = {key: value / area  for key, value in Landau_energy_drop.items()}
-		#for i in Landau_energy_drop:
-		#	cov =  i.split( "_" )[ -1 ] 
-		#	Landau_energy_per_H[ i ] = Landau_energy_drop[ i ] /  self.number_of_H[ cov ]
-		#for (key1, value1), (key2, value2) in zip( Landau_energy_drop.items(), Landau_energy_per_H.items() ):
-		#	print( key1, value1, key2, value2 )
-		return Landau_energy_per_area #Landau_energy_per_H #Landau_energy_drop
+				for pH in range( self.pH_down, self.pH_up + 1 ):
+					Landau_energy[ str( pH ) + "_" + str( SHE ) + "_" + str( coverage ) ] = self.grand_canonical_energy[ "chg_" + str( SHE ) + "_" + str(coverage) ] - self.grand_canonical_energy[ "chg_" + str( SHE ) + "_0ML"  ] - self.number_of_H[ str( coverage ) ] * self.mu_H[ str( pH ) + "_" + str( SHE )  ]        
+		Landau_energy_drop = { key: value for key, value in Landau_energy.items() if key not in self.keys_to_drop }
+		Landau_energy_per_area = {key: value / self.area  for key, value in Landau_energy_drop.items()}
+		return Landau_energy_per_area 
+
 
 	
 	def get_Landau_energy_for_pH_7( self ):
-		area = self.get_unit_cell_area()
-		keys_to_drop = list()
-		grand_canonical_energy = self.get_grand_canonical_energy()
 		Landau_energy = {}
-		Landau_energy_per_H = {}
-		energy_H2 = self.get_omega_H2()
 		for SHE in self.SHE_voltages:
 			for coverage in self.coverage_dir:     
 				for pH in [ 7 ]:                 
-					Landau_energy[ str( pH ) + "_" + str( SHE ) + "_" + str( coverage ) ] = grand_canonical_energy[ "chg_" + str( SHE ) + "_" + str(coverage) ] - grand_canonical_energy[ "chg_" + str( SHE ) + "_0ML"  ] - self.mu_H[ str( pH ) + "_" + str(SHE) + "_" + str(coverage) ]        
-		for pH in range( self.pH_down, self.pH_up + 1 ):
-			for SHE in self.SHE_voltages: 
-				keys_to_drop.append( str( pH ) + "_" + str( SHE ) + "_" + "0ML" )
-		Landau_energy_drop = { key: value for key, value in Landau_energy.items() if key not in keys_to_drop }
-		#Landau_energy_per_area = {key: value / area  for key, value in Landau_energy_drop.items()}
-		
-		Landau_energy_drop_pH_7 = {}
-		for key, value in Landau_energy_drop.items():
-			print( key, value )
+					Landau_energy[ str( pH ) + "_" + str( SHE ) + "_" + str( coverage ) ] = self.grand_canonical_energy[ "chg_" + str( SHE ) + "_" + str(coverage) ] - self.grand_canonical_energy[ "chg_" + str( SHE ) + "_0ML"  ] - self.number_of_H[ str( coverage ) ] * self.mu_H[ str( pH ) + "_" + str( SHE )  ]        
+		Landau_energy_drop = { key: value for key, value in Landau_energy.items() if key not in self.keys_to_drop }
+		Landau_energy_per_area = {key: value / self.area  for key, value in Landau_energy_drop.items()}
+
+		Landau_energy_pH_7 = {}
+		for key, value in Landau_energy_per_area.items():
 			coverage = key.split('_')[-1].replace( "ML", "" )
-			if coverage not in Landau_energy_drop_pH_7:
-				Landau_energy_drop_pH_7[ coverage ] = list()
-			Landau_energy_drop_pH_7[ coverage ].append( value )
-		for i, j in Landau_energy_drop_pH_7.items():
-			print(i, j )
-		return Landau_energy_drop_pH_7 #Landau_energy_per_H #Landau_energy_drop
+			if coverage not in Landau_energy_pH_7:
+				Landau_energy_pH_7[ coverage ] = list()
+			Landau_energy_pH_7[ coverage ].append( value )
+		for i, j in Landau_energy_pH_7.items():
+			print( i, j )
+		return Landau_energy_pH_7 
 	
-
-
-	def get_Landau_Energy_2( self ):
-		diff = {}
-		grand_canonical_energy = self.get_grand_canonical_energy()
-		for SHE in self.SHE_voltages:
-			for coverage in self.coverage_dir:
-				for pH in range( self.pH_down, self.pH_up + 1 ):
-					diff[ str( pH ) + "_" + str( SHE ) + "_" + str( coverage ) ] = grand_canonical_energy[ "chg_" + str( SHE ) + "_" + str(coverage) ]	- grand_canonical_energy[ "chg_" + str( SHE ) + "_0ML"  ] 
-		for i, j in diff.items():
-			print(i, j)
 		
 	def get_min_Landau_Energy( self, voltage, pH ):
 		min_val = math.inf    
@@ -198,12 +168,6 @@ class Landau_Energy:
 		for pH in range( 0, self.pH_up + 1 ):
 			df = self.get_data_as_DataFrame( pH = pH )
 			data[ "pH=" + str(pH) ] = df
-			#all_dfs.append( df )
-		#print( all_dfs  )		
-		#for key, value in data.items():
-		#	print( key )
-		#	print( value )
-		#	print("---" * 10)
 		return data
 		
 
@@ -254,17 +218,9 @@ class fix_data():
 		pH = np.array(data_df['pH']).reshape( self.up_pH + 1,  len( V ) )
 		Landau_Energy = np.array(data_df['Landau_Energy']).reshape( self.up_pH + 1,  len( V ) )
 		V = np.array(data_df['V']).reshape( self.up_pH + 1,  len( V ) )
-		#print( pH, V, Landau_Energy )
 		return pH, V, Landau_Energy
 
 if __name__ == "__main__":
 	obj = Landau_Energy( pH_down = 0, pH_up = 7 )
-	#obj.get_grand_canonical_energy()
-	#obj.get_Landau_Energy_2()
-	#obj.get_Landau_energy_meth_2()
-	#a = obj.get_Landau_energy_for_pH_0()
-	#print( obj.coverage_dir)
-	#print( a )
-	#obj.get_all_data_in_dictionary()
-	#obj = fix_data( 0, 7 )
-	#obj.get_plotting_data()
+	a = obj.get_all_data_in_dictionary()
+	b = obj.get_Landau_energy_for_pH_7()
