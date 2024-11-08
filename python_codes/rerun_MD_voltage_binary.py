6103abdfb029a036d168fa25d25d4ef1e7ad7e0a
theodorosP <teosfp@hotmail.com>
2024-11-08 11:05:16 -0500

    commit

--- python_codes/rerun_MD_voltage_binary.py
+++ python_codes/rerun_MD_voltage_binary.py
@@ -9,81 +9,77 @@ def get_path_and_dirs():
     return path, dirs
 
 def make_dirs_and_copy():
-	dirs = [ i for i in os.listdir( "." ) if os.path.isdir( i ) == True  ]
-	if dirs:
-		new_dir_name = f"RUN{len(dirs) + 1 }"
-	else:
-		new_dir_name = "RUN1"
-	os.mkdir( new_dir_name )
-	new_dirs = [ i for i in os.listdir( "." ) if os.path.isdir( i ) == True  ]
-	new_dirs.sort( key = os.path.getctime )
-	target_dir =  new_dirs[ - 1 ]
-	files = [ i for i in glob.glob( "*" ) if os.path.isfile( i ) ]
-	for i in files:
-		if i in [ "OUTCAR", "REPORT", "vasprun.xml", "XDATCAR" ]:
-			os.system( f"gzip {i}" )
-	files = [ i for i in glob.glob( "*" ) if os.path.isfile( i ) ]
-	for i in files:
-		shutil.copy( i, target_dir )
-	os.remove( "POSCAR" )
-	os.rename( "CONTCAR", "POSCAR" )
+    dirs = [ i for i in os.listdir( "." ) if os.path.isdir( i ) == True  ]
+    if dirs:
+        new_dir_name = f"RUN{len(dirs) + 1 }"
+    else:
+        new_dir_name = "RUN1"
+    os.mkdir( new_dir_name )
+    new_dirs = [ i for i in os.listdir( "." ) if os.path.isdir( i ) == True  ]
+    new_dirs.sort( key = os.path.getctime )
+    target_dir =  new_dirs[ - 1 ]
+    files = [ i for i in glob.glob( "*" ) if os.path.isfile( i ) ]
+    for i in files:
+        if i in [ "OUTCAR", "REPORT", "vasprun.xml", "XDATCAR" ]:
+            os.system( f"gzip {i}" )
+    files = [ i for i in glob.glob( "*" ) if os.path.isfile( i ) ]
+    for i in files:
+        shutil.copy( i, target_dir )
+    for i in [ "OUTCAR.gz", "REPORT.gz", "vasprun.xml.gz", "XDATCAR.gz", "POSCAR" ]:
+        os.remove( i )
+    os.rename( "CONTCAR", "POSCAR" )
 
 def get_OUTCAR_NELECT():
-	files = [ i for i in glob.glob( "*" ) if os.path.isfile( i ) ]
-	#print( files )
-	if "OUTCAR" in files:
-		print( "OUTCAR found" )
-		with open( "OUTCAR", "r" ) as file:
-			lines = file.readlines()
-		nelect_lines = [ i for i in lines if "NELECTCURRENT" in i ]
-		last_line = nelect_lines[ - 1 ].strip()
-		#print( last_line )
-		if nelect_lines:
-			match =  re.search( r'NELECTCURRENT\s+([\d.-]+)', last_line )
-			#print( "NELECTCURRENT = ", match.group( 1 )  )
-			return  float( match.group( 1 ) )
-		else:
-			print( "NELECT not found yet" )
-			return
-	else:
-		print( "OUTCAR not found" )
-		return
-
+    files = [ i for i in glob.glob( "*" ) if os.path.isfile( i ) ]
+    if "OUTCAR" in files:
+        print( "OUTCAR found" )
+        with open( "OUTCAR", "rb" ) as file:
+            lines = file.readlines()
+        nelect_lines = [line.decode('utf-8', errors='ignore').strip() for line in lines if b"NELECTCURRENT" in line]
+        last_line = nelect_lines[ - 1 ].strip()
+        if nelect_lines:
+            match =  re.search( r'NELECTCURRENT\s+([\d.-]+)', last_line )
+            return  float( match.group( 1 ) )
+        else:
+            print( "NELECT not found yet" )
+            return
+    else:
+        print( "OUTCAR not found" )
+        return
 
 def update_INCAR( val ):
-	with open( "INCAR", 'r') as file:
-		lines = file.readlines()
+    with open( "INCAR", 'r') as file:
+        lines = file.readlines()
 
-	NELECT_present = False
-	for i, line in enumerate( lines ):
-		if line.startswith( 'NELECT' ):
-			lines[ i ] = f'NELECT = {val}\n'
-			NELECT_present = True
-			break
+    NELECT_present = False
+    for i, line in enumerate( lines ):
+        if line.startswith( 'NELECT' ):
+            lines[ i ] = f'NELECT = {val}\n'
+            NELECT_present = True
+            break
 
-	if not NELECT_present:
-		lines.append(f'NELECT = {val}\n')
+    if not NELECT_present:
+        lines.append(f'NELECT = {val}\n')
 
-	with open( "INCAR" , 'w' ) as file:
-		file.writelines( lines )
-
+    with open( "INCAR" , 'w' ) as file:
+        file.writelines( lines )
 
 def parse_INCAR():
-	path, dirs = get_path_and_dirs()
-	for i in dirs:
-		os.chdir( path + "/" + i  )
-		print( "--" * 10 )
-		print( os.getcwd()  )
-		NELECTCURRENT = get_OUTCAR_NELECT()
-		print( "nelect =", NELECTCURRENT )
-		make_dirs_and_copy()
-		update_INCAR( NELECTCURRENT )
+    path, dirs = get_path_and_dirs()
+    for i in dirs:
+        os.chdir( path + "/" + i  )
+        print( "--" * 10 )
+        print( os.getcwd()  )
+        NELECTCURRENT = get_OUTCAR_NELECT()
+        print( "nelect =", NELECTCURRENT )
+        make_dirs_and_copy()
+        update_INCAR( NELECTCURRENT )
 
 def submit_job():
-	cpath = os.getcwd()
-	parse_INCAR()
-	os.chdir( cpath )
-	os.system( "sbatch job" )
+    cpath = os.getcwd()
+    parse_INCAR()
+    os.chdir( cpath )
+    os.system( "sbatch job" )
 
 if __name__ == "__main__":
-	submit_job()
+    submit_job()
