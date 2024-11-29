afc690c7e6ce9a87da4050466d93682736adfe88
thodorisP <teosfp@hotmail.com>
2024-11-29 10:57:17 -0500

    commit

--- bash_codes/find_file.sh
+++ bash_codes/find_file.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 
-find ~/ -name dlePy.vasp.outcar2traj 2>/dev/null
+find ~/ -name dlePy.vasp.outcar2traj 2>/dev/null #for file
 
-
+find / -type d -iname "MOVIE" 2>/dev/null #for folder
